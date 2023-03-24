7af23b78f99a38ce2d0c3df32924806d4ea0c777
rubailly-cure <bailly.rurangirwa@cureinternational.org>
2023-03-25 00:02:55 +0200

    Fixing display value for assessment and plan columns

--- omod/src/test/java/org/openmrs/module/cchereports/TestRegex.java
+++ omod/src/test/java/org/openmrs/module/cchereports/TestRegex.java
@@ -4,25 +4,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TestRegex {
-
+	
 	public static void main(String[] args) {
-
+		
 		String input = "tthen tan";
-
+		
 		Pattern pattern = Pattern.compile("t.*n");
 		Matcher matcher = pattern.matcher(String.valueOf(input));
 		System.out.println(matcher.find());
 		System.out.println(matcher.group());
-
+		
 		/*
 		 * while (matcher.find()) { System.out.println(matcher.group() + "Starts at " +
 		 * matcher.start() + " and ends at " + matcher.end());
 		 * 
 		 * }
 		 */
-
+		
 	}
-
+	
 	/*
 	 * public static void main(String[] args) {
 	 * 
@@ -35,5 +35,5 @@ public class TestRegex {
 	 * 
 	 * }
 	 */
-
+	
 }
